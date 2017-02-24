d5523fcfa7d766b78bcd72869cb2aadef1e0baf8
fccaikai <714564390@qq.com>
2017-02-24 15:34:20 +0800

    add BaseActivity

---
+++ baselib/src/main/java/com/kcode/baselib/base/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.kcode.baselib.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by caik on 2017/2/24.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
